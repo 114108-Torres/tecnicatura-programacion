a7b4cb9e0d1f0147f810446f617ab0716d2a8545
114108_Eric_Torres <114108@tecnicatura.frc.utn.edu.ar>
2025-05-10 20:43:04 -0300

    ejercicio 2 resuelto

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int CantidadArticulos = 3;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese el precio del artículo: $");
-        double precio = scanner.nextDouble();
+        double subTotal = 0;
 
-        double precioFinal = precio * 1.21;
+        for (int i = 1  ; i <= CantidadArticulos; i++) {
+            System.out.println("Ingrese el precio del articulo" + i +  " $ ");
+            double precio = scanner.nextDouble();
+
+            System.out.println("Ingrese la cantidad del articulo" + i );
+            int cantidad = scanner.nextInt();
+
+            subTotal += precio * cantidad;
+        }
+
+        double precioFinal = subTotal * 1.21;
         double total = Math.round(precioFinal * 100.0) / 100.0;
 
-        System.out.println("El precio final con IVA 21% incluído es de $" + total );
+        System.out.println("El precio final con IVA 21% de la factura es de $" + total );
 
     }
 }
