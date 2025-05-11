9246e33e513a3db4be6cb4fbdfe6d238d082c326
114108_Eric_Torres <114108@tecnicatura.frc.utn.edu.ar>
2025-05-11 11:01:04 -0300

    ejercicio 11 resuelto

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,31 @@
 import java.util.Scanner;
 
-/* 10. Ingresar un número y validar que sea positivo.
-Si no lo es continuar solicitándolo al usuario hasta que ingrese un número correcto.
+/* 11. Ingresar el tiempo en segundos que realizó el ganador de la clasificación de una carrera de F1.
+Luego ingresar los tiempos de los otros 9 corredores e informar cuántos disputarán la carrera.
+Para que un corredor pueda participar su tiempo de clasificación no puede superar en 15 % al del ganador.
  */
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int TOTAL_CORREDORES = 9;
+        int contador = 0;
 
-        System.out.print("Ingrese un número: ");
-        int num = sc.nextInt();
+        System.out.print("Ingrese el tiempo en segundos del ganador de la clasificación de la F1 :");
+        int segGanador = sc.nextInt();
 
-        while   (num < 0)
-        {
-            System.out.println("El número "+num+" no es positivo. Ingrese un número mayor que 0");
-            System.out.print("Ingrese un número: ");
-            num = sc.nextInt();
+        for (int i = 1; i <= TOTAL_CORREDORES; i++) {
+            System.out.print("Ingrese el tiempo en segundos del corredor " + i + " : ");
+            int segCorredor = sc.nextInt();
+            if (CalcularTiempos(segGanador,segCorredor)) {
+                contador++;
+            }
         }
-        System.out.println("El número "+num+" es positivo. Bien hecho!");
+        System.out.print("La cantidad de corredores que participan de la carrera es : " + contador + " pilotos (no superaron el 15% del tiempo del ganador).");
+
+    }
+
+    public static boolean CalcularTiempos (int tiempoGanador, int tiempoCorredor) {
+        return tiempoCorredor < (tiempoGanador * 1.15);
     }
 }
