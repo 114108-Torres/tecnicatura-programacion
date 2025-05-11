410ac36deab7d1bb1f6703248885c5730bf69f03
114108_Eric_Torres <114108@tecnicatura.frc.utn.edu.ar>
2025-05-11 11:17:58 -0300

    ejercicio 12 resuelto

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
 
-/* 11. Ingresar el tiempo en segundos que realizó el ganador de la clasificación de una carrera de F1.
-Luego ingresar los tiempos de los otros 9 corredores e informar cuántos disputarán la carrera.
-Para que un corredor pueda participar su tiempo de clasificación no puede superar en 15 % al del ganador.
+/* 12. Desarrollar un programa que permita ingresar la antigüedad los autos de una concesionaria de usados.
+Finalizar la carga cuando se ingrese 0. Por cada auto ingresado mostrar por pantalla un mensaje adecuado
+con el siguiente criterio: si el auto tiene entre 1 y 5 años mostrar el texto “NUEVO”,
+si tiene entre 6 y 10 mostrar “POCO USO”, si tiene entre 11 y 20 “MUCHO USO” y si tiene más de 20 “MUY ANTIGUO”.
  */
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int TOTAL_CORREDORES = 9;
-        int contador = 0;
 
-        System.out.print("Ingrese el tiempo en segundos del ganador de la clasificación de la F1 :");
-        int segGanador = sc.nextInt();
+        System.out.print("Ingrese la cantidad de autos a cargar en sistema: ");
+        int totalAutos = sc.nextInt();
 
-        for (int i = 1; i <= TOTAL_CORREDORES; i++) {
-            System.out.print("Ingrese el tiempo en segundos del corredor " + i + " : ");
-            int segCorredor = sc.nextInt();
-            if (CalcularTiempos(segGanador,segCorredor)) {
-                contador++;
+        for (int i = 1; i <= totalAutos; i++) {
+            System.out.print("Ingrese la antigüedad del auto " + i + ": ");
+            int antigAuto = sc.nextInt();
+            if (antigAuto >= 1 && antigAuto <= 5) {
+                System.out.println("El auto "+ i +" es NUEVO.");
+            } else if (antigAuto > 5 && antigAuto <= 10) {
+                System.out.println("El auto "+ i +" tiene POCO USO.");
+            } else if (antigAuto > 10 && antigAuto <= 20) {
+                System.out.println("El auto "+ i +" tiene MUCHO USO.");
+            } else if (antigAuto > 20 ) {
+                System.out.println("El auto "+ i +" es MUY ANTIGUO.");
             }
         }
-        System.out.print("La cantidad de corredores que participan de la carrera es : " + contador + " pilotos (no superaron el 15% del tiempo del ganador).");
 
-    }
 
-    public static boolean CalcularTiempos (int tiempoGanador, int tiempoCorredor) {
-        return tiempoCorredor < (tiempoGanador * 1.15);
     }
 }
