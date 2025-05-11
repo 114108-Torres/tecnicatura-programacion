8f0c4f6fe49163ce5d4b4408dfc823b1ab582216
114108_Eric_Torres <114108@tecnicatura.frc.utn.edu.ar>
2025-05-11 12:45:46 -0300

    ejercicio 13 resuelto

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 
-/* 12. Desarrollar un programa que permita ingresar la antigüedad los autos de una concesionaria de usados.
-Finalizar la carga cuando se ingrese 0. Por cada auto ingresado mostrar por pantalla un mensaje adecuado
-con el siguiente criterio: si el auto tiene entre 1 y 5 años mostrar el texto “NUEVO”,
-si tiene entre 6 y 10 mostrar “POCO USO”, si tiene entre 11 y 20 “MUCHO USO” y si tiene más de 20 “MUY ANTIGUO”.
+/* 13. Luego de finalizada la carga calcular y mostrar los siguientes resultados:
+• Cantidad total de autos.
+• Cantidad de autos con poco uso.
+• Promedio de antigüedad de los autos que no sean muy antiguos
  */
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int pocoUso = 0;
+        int sumaAntiguedades = 0;
+        int contador = 0;
 
         System.out.print("Ingrese la cantidad de autos a cargar en sistema: ");
         int totalAutos = sc.nextInt();
@@ -16,17 +20,35 @@ public class Main {
         for (int i = 1; i <= totalAutos; i++) {
             System.out.print("Ingrese la antigüedad del auto " + i + ": ");
             int antigAuto = sc.nextInt();
+
             if (antigAuto >= 1 && antigAuto <= 5) {
-                System.out.println("El auto "+ i +" es NUEVO.");
+                System.out.println("El auto " + i + " es NUEVO.");
+                sumaAntiguedades += antigAuto;
+                contador++;
             } else if (antigAuto > 5 && antigAuto <= 10) {
-                System.out.println("El auto "+ i +" tiene POCO USO.");
+                System.out.println("El auto " + i + " tiene POCO USO.");
+                pocoUso++;
+                sumaAntiguedades += antigAuto;
+                contador++;
             } else if (antigAuto > 10 && antigAuto <= 20) {
-                System.out.println("El auto "+ i +" tiene MUCHO USO.");
-            } else if (antigAuto > 20 ) {
-                System.out.println("El auto "+ i +" es MUY ANTIGUO.");
+                System.out.println("El auto " + i + " tiene MUCHO USO.");
+                sumaAntiguedades += antigAuto;
+                contador++;
+            } else if (antigAuto > 20) {
+                System.out.println("El auto " + i + " es MUY ANTIGUO.");
             }
+
+            System.out.println(); // Salto de línea entre autos
         }
 
+        System.out.println("La cantidad total de autos es: " + totalAutos);
+        System.out.println("La cantidad de autos con POCO USO es: " + pocoUso);
 
+        if (contador > 0) {
+            double promedio = (double) sumaAntiguedades / contador;
+            System.out.println("El promedio de antigüedad de los autos no MUY ANTIGUOS es de: " + promedio + " años.");
+        } else {
+            System.out.println("No se ingresaron autos que no sean MUY ANTIGUOS.");
+        }
     }
 }
