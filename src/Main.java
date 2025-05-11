254c32a64a39be566a1a6ba190a5ad7f789dc9b2
114108_Eric_Torres <114108@tecnicatura.frc.utn.edu.ar>
2025-05-10 22:41:15 -0300

    ejercicio 5 resuelto

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,31 @@
 import java.util.Scanner;
 
+/* 5. Ingresar la cantidad de horas trabajadas por un empleado y el sueldo que cobra por hora.
+ Indique el total a cobrar teniendo en cuenta que si trabajó más de 180 horas las excedentes las cobra con un 50 % de aumento.
+ */
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double altura1;
-        double altura2;
-        String nombre1;
-        String nombre2;
+        double sueldo;
+        int hsTrabajo;
+        double sueldoTrabajo;
 
-            System.out.println("Ingrese el nombre de la primer persona" );
-            nombre1 = scanner.next();
-            System.out.println("Ingrese la altura de la primer persona" );
-            altura1 = scanner.nextDouble();
+        System.out.println("Ingrese la cantidad de horas trabajados por el empleado: " );
+        hsTrabajo = scanner.nextInt();
+        System.out.println("Ingrese el sueldo por hora: $" );
+        sueldo = scanner.nextDouble();
 
-            System.out.println("Ingrese el nombre de la segunda persona" );
-             nombre2 = scanner.next();
-            System.out.println("Ingrese la altura de la segunda persona" );
-            altura2 = scanner.nextDouble();
-
-            if (altura1 > altura2) {
-                System.out.println("La persona más alta es: " +  nombre1);
-            } else {
-                System.out.println("La persona más alta es: " +  nombre2);
-            }
+        if (hsTrabajo < 180) {
+            sueldoTrabajo = Math.round(sueldo * hsTrabajo) ;
+            System.out.println("Salario a percibir: $ " + sueldoTrabajo);
+        } else if (hsTrabajo > 180) {
+            sueldoTrabajo = Math.round(sueldo * 180) ;
+            int hsExcedente = hsTrabajo - 180;
+            double sueldoExcedente = (sueldo * 1.5) * hsExcedente;
+            double sueldoConExtras = sueldoTrabajo + sueldoExcedente;
+            System.out.println("Salario a percibir: $ " + sueldoConExtras);
+        }
 
 
     }
