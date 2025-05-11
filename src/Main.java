ab619f7c8aab6ecdb2fbafffb56a672da0e76d26
114108_Eric_Torres <114108@tecnicatura.frc.utn.edu.ar>
2025-05-10 22:44:43 -0300

    ejercicio 5 resuelto

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+         int HORAS_BASE = 180;
+         double EXTRA_MULTIPLICADOR = 1.5;
+
         Scanner scanner = new Scanner(System.in);
-        double sueldo;
-        int hsTrabajo;
-        double sueldoTrabajo;
 
-        System.out.println("Ingrese la cantidad de horas trabajados por el empleado: " );
-        hsTrabajo = scanner.nextInt();
-        System.out.println("Ingrese el sueldo por hora: $" );
-        sueldo = scanner.nextDouble();
+        System.out.print("Ingrese la cantidad de horas trabajadas por el empleado: ");
+        int horasTrabajadas = scanner.nextInt();
 
-        if (hsTrabajo < 180) {
-            sueldoTrabajo = Math.round(sueldo * hsTrabajo) ;
-            System.out.println("Salario a percibir: $ " + sueldoTrabajo);
-        } else if (hsTrabajo > 180) {
-            sueldoTrabajo = Math.round(sueldo * 180) ;
-            int hsExcedente = hsTrabajo - 180;
-            double sueldoExcedente = (sueldo * 1.5) * hsExcedente;
-            double sueldoConExtras = sueldoTrabajo + sueldoExcedente;
-            System.out.println("Salario a percibir: $ " + sueldoConExtras);
+        System.out.print("Ingrese el sueldo por hora: $");
+        double sueldoPorHora = scanner.nextDouble();
+
+        double salarioTotal;
+
+        if (horasTrabajadas <= HORAS_BASE) {
+            salarioTotal = horasTrabajadas * sueldoPorHora;
+        } else {
+            int horasExtra = horasTrabajadas - HORAS_BASE;
+            salarioTotal = (HORAS_BASE * sueldoPorHora) + (horasExtra * sueldoPorHora * EXTRA_MULTIPLICADOR);
         }
 
+        salarioTotal = Math.round(salarioTotal * 100.0) / 100.0;
 
+        System.out.println("Salario a percibir: $" + salarioTotal);
     }
 }
