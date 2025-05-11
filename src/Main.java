fcce0d91ecf2246cb85d4b7b34d626f22d14918d
114108_Eric_Torres <114108@tecnicatura.frc.utn.edu.ar>
2025-05-11 10:38:43 -0300

    ejercicio 10 resuelto

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,22 @@
 import java.util.Scanner;
 
-/* 9. Ingresar un número n y a continuación n números positivos.
-Informar la cantidad de mayores a 5 que se hayan ingresado.
+/* 10. Ingresar un número y validar que sea positivo.
+Si no lo es continuar solicitándolo al usuario hasta que ingrese un número correcto.
  */
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("¿Cuántos números desea ingresar?: ");
-        int cantidadNumeros = sc.nextInt();
+        System.out.print("Ingrese un número: ");
+        int num = sc.nextInt();
 
-        int mayoresA5 = 0;
-
-        for (int i = 1; i <= cantidadNumeros; i++) {
-            System.out.print("Ingrese un número positivo (" + i + " de " + cantidadNumeros + "): ");
-            int numero = sc.nextInt();
-
-            if (numero > 0 && numero > 5) {
-                mayoresA5++;
-            } else if (numero <= 0) {
-                System.out.println("⚠️ El número debe ser positivo. No se contabiliza.");
-                i--; // Repetir esta vuelta
-            }
+        while   (num < 0)
+        {
+            System.out.println("El número "+num+" no es positivo. Ingrese un número mayor que 0");
+            System.out.print("Ingrese un número: ");
+            num = sc.nextInt();
         }
-
-        System.out.println("Se ingresaron " + mayoresA5 + " números mayores a 5.");
+        System.out.println("El número "+num+" es positivo. Bien hecho!");
     }
 }
